99c2470f2392ff4bb8122a990281a86456ea5bc5
João Thallis <joaothallis.developer@gmail.com>
2020-03-10 18:57:41 -0300

    wip

--- config/dev.exs
+++ config/dev.exs
@@ -3,7 +3,7 @@ use Mix.Config
 # Configure your database
 config :healthyskin, Healthyskin.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "docker",
   database: "healthyskin_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
