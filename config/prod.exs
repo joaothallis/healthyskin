938d06f270e1637d69c709864b79f79cef5c1579
João Thallis <joaothallis.developer@gmail.com>
2020-03-11 00:27:09 -0300

    Heroku

--- config/prod.exs
+++ config/prod.exs
@@ -10,8 +10,8 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :healthyskin, HealthyskinWeb.Endpoint,
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [scheme: "https", host: "example.com", port: 433],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]]
 
 # Do not print debug messages in production
 config :logger, level: :info
